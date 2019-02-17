b2a180fb1c729977d02f4ed0239b68854b99e09f
TomFelix <tomasz.agacinski@gmail.com>
2019-02-18 00:45:49 +0100

    portfolio website

---
+++ src/pages/portfolio.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Portfolio extends Component {
+    render() {
+      return (
+        <div className="portfolio">
+            <h1>Portfolio</h1>
+            <p>portfolio</p>
+        </div>
+      );
+    }
+  }
