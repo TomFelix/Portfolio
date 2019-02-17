5f60eec4d63a104e85467c2d3978c9a673924f14
TomFelix <tomasz.agacinski@gmail.com>
2019-02-18 00:51:04 +0100

    Updates

---
+++ precache-manifest.af03c2086b638acc9a0bdb7936b8799c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eec6feb9b19e46b66dae",
+    "url": "/static/js/main.eec6feb9.chunk.js"
+  },
+  {
+    "revision": "b0e515e9d16dec91769a",
+    "url": "/static/js/1.b0e515e9.chunk.js"
+  },
+  {
+    "revision": "eec6feb9b19e46b66dae",
+    "url": "/static/css/main.aec05570.chunk.css"
+  },
+  {
+    "revision": "2cf6ebf246406615e9966d94e254cd5a",
+    "url": "/index.html"
+  }
+];
